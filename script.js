d3e0f5eb212dbf1959a65d822c3a2672d5cd3a45
William Valadez <billy.valadez@icloud.com>
2022-07-14 20:33:27 -0400

    new commint

--- script.js
+++ script.js
@@ -1,20 +1,48 @@
-console.log('hello');
 
-
-// Require
-const request = require('request');
-
-// Request
-request('https://appointmentthing.com/api/v1/appointments/list/?token=api1657322026ENuhMImlrwAv3oXf7xDO169472&type=TYPE', function(err, res, dat){
-
-    // Parse
-    var data = JSON.parse(dat);
-
-    // Request OK?
-    if(data.meta.code == '200'){
-
-        // Log
-        console.log(data);
-
+fetch("https://www.thecocktaildb.com/api/json/v1/1/random.php")
+  .then((response) => {
+    if (response.ok) {
+      return response.json();
+    } else {
+      throw new Error("NETWORK RESPONSE ERROR");
     }
-});
+  })
+  .then(data => {
+    console.log(data);
+    displayCocktail(data)
+  })
+  .catch((error) => console.error("FETCH ERROR:", error));
+
+  function displayCocktail(data) {
+    const cocktail = data.drinks[0];
+    const cocktailDiv = document.getElementById("cocktail");
+    const cocktailName = cocktail.strDrink;
+    const heading = document.createElement("h1");
+    heading.innerHTML = cocktailName;
+    cocktailDiv.appendChild(heading);
+    const cocktailImg = document.createElement("img");
+    cocktailImg.src = cocktail.strDrinkThumb;
+    cocktailDiv.appendChild(cocktailImg);
+    document.body.style.backgroundImage = "url('" + cocktail.strDrinkThumb + "')";
+
+    const cocktailIngredients = document.createElement("ul");
+    cocktailDiv.appendChild(cocktailIngredients);  
+    
+    const getIngredients = Object.keys(cocktail)
+      .filter(function (ingredient) {
+        return ingredient.indexOf("strIngredient") == 0;
+      })
+      .reduce(function (ingredients, ingredient) {
+        if (cocktail[ingredient] != null) {
+          ingredients[ingredient] = cocktail[ingredient];
+        }
+        return ingredients;
+      }, {});
+  
+    for (let key in getIngredients) {
+      let value = getIngredients[key];
+      listItem = document.createElement("li");
+      listItem.innerHTML = value;
+      cocktailIngredients.appendChild(listItem);
+    }
+  }   
